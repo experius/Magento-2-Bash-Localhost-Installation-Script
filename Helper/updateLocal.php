85ae2dd2309f1a532c144275e7a1b7fe136ba45f
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2018-04-01 21:45:16 +0200

    [TASK] added local.xml modifier for Magento 1

--- Helper/updateLocal.php
+++ Helper/updateLocal.php
@@ -4,9 +4,19 @@ $config = getopt('f:d:u:p:');
 
 $envFilePath = $config['f'] . '/app/etc/local.xml';
 
-$dom = new DOMDocument();
-$dom->load($envFilePath);
+$localXml = new SimpleXMLElement(file_get_contents($envFilePath));
 
-$dom->save($config['f'] . "/app/local-test.xml");
+
+$localXml->global->resources->default_setup->connection->host = '<![CDATA[' . '127.0.0.1' . ']]>';
+$localXml->global->resources->default_setup->connection->dbname = '<![CDATA[' . $config['d'] . ']]>';
+$localXml->global->resources->default_setup->connection->username = '<![CDATA[' . $config['u'] . ']]>';
+$localXml->global->resources->default_setup->connection->password = '<![CDATA[' . $config['p'] . ']]>';
+
+file_put_contents(
+    $config['f'] . "/app/etc/local.xml",
+    html_entity_decode($localXml->asXML(), ENT_NOQUOTES, 'UTF-8')
+);
+
+
 
 ?>
