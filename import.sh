352c54d968a509557fb4fe97f2c971ef8f4088a7
Lewis Voncken <lewis@experius.nl>
2018-04-09 12:50:06 +0200

    [TASK] Exclude pub/media/catalog/product/* (and for m1 media/catalog/product/*) , var/log/*, var/report/*

--- import.sh
+++ import.sh
@@ -19,7 +19,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 ## Unpack the files
-tar -xvf $DIRECTORY/files.tar --directory $DIRECTORY
+tar -xvf $DIRECTORY/files.tar --directory $DIRECTORY --exclude=pub/media/catalog/product/* --exclude=media/catalog/product/* --exclude=var/log/* --exclude=var/report/*
 
 ## Create and import DB
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE_NAME\`"
