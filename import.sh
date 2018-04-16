926e275946378b6e3519358d94afe9175cef945d
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2018-04-16 12:12:24 +0200

    [BUGFIX] Different Magerun create admin user for m1

--- import.sh
+++ import.sh
@@ -69,4 +69,9 @@ rm $DIRECTORY/data.sql
 
 ## Delete Current Admin User and Create New Admin User
 $MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:delete $MAGENTO_USERNAME
-$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:create --admin-user $MAGENTO_USERNAME --admin-password $MAGENTO_PASSWORD --admin-email $MAGENTO_USER_EMAIL --admin-firstname $MAGENTO_USERNAME --admin-lastname $MAGENTO_USERNAME
+
+if [ "$VERSION" = "m2" ]; then
+	$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:create --admin-user $MAGENTO_USERNAME --admin-password $MAGENTO_PASSWORD --admin-email $MAGENTO_USER_EMAIL --admin-firstname $MAGENTO_USERNAME --admin-lastname $MAGENTO_USERNAME
+else
+	$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:create $MAGENTO_USERNAME $MAGENTO_USER_EMAIL $MAGENTO_PASSWORD $MAGENTO_USERNAME $MAGENTO_USERNAME
+fi
