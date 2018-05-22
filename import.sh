62977dc83f237c51b26bb5a89b83a9cb99534f88
Lewis Voncken <lewis@experius.nl>
2018-05-22 11:24:45 +0200

    [TASK] Added force delete admin user

--- import.sh
+++ import.sh
@@ -79,7 +79,7 @@ rm $DIRECTORY/structure.sql
 rm $DIRECTORY/data.sql
 
 ## Delete Current Admin User and Create New Admin User
-$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:delete $MAGENTO_USERNAME
+$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:delete $MAGENTO_USERNAME -f
 
 if [ "$VERSION" = "m2" ]; then
 	$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:create --admin-user $MAGENTO_USERNAME --admin-password $MAGENTO_PASSWORD --admin-email $MAGENTO_USER_EMAIL --admin-firstname $MAGENTO_USERNAME --admin-lastname $MAGENTO_USERNAME
