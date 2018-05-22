0e9809d44805161a5f7696ca059ab698735f14e4
Lewis Voncken <lewis@experius.nl>
2018-05-22 10:59:48 +0200

    [BUGFIX] Solved problem with the admin user already exists by deleting it for Magento 1

--- import.sh
+++ import.sh
@@ -73,5 +73,6 @@ $MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:delete $MAGENTO_USERNAME
 if [ "$VERSION" = "m2" ]; then
 	$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:create --admin-user $MAGENTO_USERNAME --admin-password $MAGENTO_PASSWORD --admin-email $MAGENTO_USER_EMAIL --admin-firstname $MAGENTO_USERNAME --admin-lastname $MAGENTO_USERNAME
 else
+	$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:delete $MAGENTO_USERNAME
 	$MAGERUN_COMMAND --root-dir=$DIRECTORY admin:user:create $MAGENTO_USERNAME $MAGENTO_USER_EMAIL $MAGENTO_PASSWORD $MAGENTO_USERNAME $MAGENTO_USERNAME
 fi
