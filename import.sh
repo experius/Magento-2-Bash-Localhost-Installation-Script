db8ceff0c5e6e4a0499ffdaa887f6b31885e9f91
Mr. Lewis <lewis@experius.nl>
2019-04-02 12:08:32 +0200

    [TASK] Skip .idea so you start with a new idea project

--- import.sh
+++ import.sh
@@ -29,7 +29,7 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 ## Unpack the files
-tar -xvf $DIRECTORY/files.tar.gz --directory $DIRECTORY -k --exclude=pub/media/catalog/product/* --exclude=media/catalog/product/* --exclude=var/log/* --exclude=var/report/*
+tar -xvf $DIRECTORY/files.tar.gz --directory $DIRECTORY -k --exclude=.idea --exclude=pub/media/catalog/product/* --exclude=media/catalog/product/* --exclude=var/log/* --exclude=var/report/*
 
 ## Create and import DB
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE_NAME\`"
