88f536ed38f97ad4bd8ea5d2af9b8d20917a0024
Cas Satter <cas.satter@experius.nl>
2020-05-12 17:10:49 +0200

    [FEATURE] - Refactor init_cloud_env.sh and make it use the update_website command with data.sql + structure.sql in $DOMAINS_PATH folder, allowing users to easily setup a local Magento Cloud environment

--- init_cloud_env.sh
+++ init_cloud_env.sh
@@ -7,40 +7,36 @@ else
     . $SCRIPTPATH/config.sample.sh
 fi
 
-$MC_COMMAND auth:info
-$MC_COMMAND ssh-key:add --yes
-
 if [ "$1" = "--help" ] || [ "$1" = "-h" ] ; then
   echo "The options available for this command:
-        mc_install 1 2 3 4
-        1: project identifier
-        2: The name of the website"
+        init_cloud_env 1 2 3
+        1: Project identifier
+        2: The name of the website
+        3: Branch to get project from (default is develop)"
   exit 0
 fi
 
+$MC_COMMAND auth:info
+$MC_COMMAND login --force
+$MC_COMMAND ssh-key:add --yes
 
 PROJECT=$1
 NAME=$2
-
+BRANCH=$3
 
 if [ -z "$NAME" ]; then
 	echo "enter name. Will be used as  $DOMAIN_PREFIX<yourname>$DOMAIN_SUFFIX"
 	exit;
 fi
 
+if [ -z "$BRANCH" ]; then
+	BRANCH="develop"
+fi
+
 VALET_DOMAIN=$DOMAIN_PREFIX$NAME
 DIRECTORY=$DOMAINS_PATH/$VALET_DOMAIN$FOLDER_SUFFIX
-DOMAIN=$VALET_DOMAIN$DOMAIN_SUFFIX
 MYSQL_DATABASE_NAME=$MYSQL_DATABASE_PREFIX$NAME
 MYSQL_DATABASE_NAME="${MYSQL_DATABASE_NAME//./_}"
-if [[ $EDITION != "custom-"* ]]; then
-    if [ -d "$DIRECTORY" ]; then
-        echo "already exists"
-        exit;
-    fi
-fi
-## Create Database
-mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE_NAME\`"
 
 COMPOSER="composer"
 PHP="php"
@@ -56,8 +52,9 @@ if [ "$VERSION" ]; then
 fi
 
 ## Download Magento
-$MC_COMMAND project:get $PROJECT $DIRECTORY
-cd $DIRECTORY
+$MC_COMMAND project:get $PROJECT $DIRECTORY -e $BRANCH
+cd $DIRECTORY || exit
+$COMPOSER install -d$DIRECTORY
 $MC_COMMAND build --no-build-hooks
 
 ## Install Sample Data
@@ -65,33 +62,21 @@ mkdir $DIRECTORY/var/composer_home
 
 ## Copy Json Auth
 if [ -e $COMPOSER_AUTH_JSON_FILE_PATH ]; then
-        cp $COMPOSER_AUTH_JSON_FILE_PATH $DIRECTORY/var/composer_home/auth.json
-fi
-
-## Install Magento
-URL="http://$DOMAIN"
-if [ "$secure" = "true" ]; then
-	URL="https://$DOMAIN"
+    cp $COMPOSER_AUTH_JSON_FILE_PATH $DIRECTORY/var/composer_home/auth.json
 fi
 
 rm -rf $DIRECTORY/generated/*
 rm -rf $DIRECTORY/var/cache/*
 
-$PHP $DIRECTORY/bin/magento setup:install --admin-firstname="$MAGENTO_USERNAME" --admin-lastname="$MAGENTO_USERNAME" --admin-email="$MAGENTO_USER_EMAIL" --admin-user="$MAGENTO_USERNAME" --admin-password="$MAGENTO_PASSWORD" --base-url="$URL" --backend-frontname="$MAGENTO_ADMIN_URL" --db-host="127.0.0.1" --db-name="$MYSQL_DATABASE_NAME" --db-user="$MYSQL_USER" --db-password="$MYSQL_PASSWORD" --language=nl_NL --currency=EUR --timezone=Europe/Amsterdam --use-rewrites=1 --session-save=files
-
+## Install Magento & Setup Database
+$PHP $DIRECTORY/bin/magento setup:install --admin-firstname="$MAGENTO_USERNAME" --admin-lastname="$MAGENTO_USERNAME" --admin-email="$MAGENTO_USER_EMAIL" --admin-user="$MAGENTO_USERNAME" --admin-password="$MAGENTO_PASSWORD" --base-url="$URL" --backend-frontname="$MAGENTO_ADMIN_URL" --db-host="127.0.0.1" --db-name="$MYSQL_DATABASE_NAME" --db-user="$MYSQL_USER" --db-password="$MYSQL_PASSWORD" --language=nl_NL --currency=EUR --timezone=Europe/Amsterdam --use-rewrites=1 --session-save=files --cleanup-database --no-interaction
 $PHP $DIRECTORY/bin/magento setup:upgrade
 
+## Default remote name is magento
+git remote rename magento origin
 
-## Developer Settings
-$PHP $DIRECTORY/bin/magento deploy:mode:set developer
-$PHP $DIRECTORY/bin/magento cache:enable
-$PHP $DIRECTORY/bin/magento cache:disable layout block_html collections full_page
-### Generated PhpStorm XML Schema Validation
-mkdir -p $DIRECTORY/.idea
-$PHP $DIRECTORY/bin/magento dev:urn-catalog:generate $DIRECTORY/.idea/misc.xml
+## Move data.sql & structure.sql to $DIRECTORY
+cp $DOMAINS_PATH/structure.sql $DIRECTORY/structure.sql
+cp $DOMAINS_PATH/data.sql $DIRECTORY/data.sql
 
-. $SCRIPTPATH/src/update_settings.sh
-
-. $SCRIPTPATH/src/secure_domain.sh
-
-. $SCRIPTPATH/src/nfs.sh
+. $SCRIPTPATH/update.sh "$NAME"
--- init_cloud_project.sh
+++ init_cloud_project.sh
@@ -7,95 +7,25 @@ else
     . $SCRIPTPATH/config.sample.sh
 fi
 
-$MC_COMMAND auth:info
-$MC_COMMAND ssh-key:add --yes
-
 if [ "$1" = "--help" ] || [ "$1" = "-h" ] ; then
   echo "The options available for this command:
-        mc_install 1 2 3 4
+        init_cloud_project 1 2
         1: project identifier
         2: The name of the website"
   exit 0
 fi
 
-
 PROJECT=$1
 NAME=$2
-
 
 if [ -z "$NAME" ]; then
 	echo "enter name. Will be used as  $DOMAIN_PREFIX<yourname>$DOMAIN_SUFFIX"
 	exit;
 fi
 
-VALET_DOMAIN=$DOMAIN_PREFIX$NAME
-DIRECTORY=$DOMAINS_PATH/$VALET_DOMAIN$FOLDER_SUFFIX
-DOMAIN=$VALET_DOMAIN$DOMAIN_SUFFIX
-MYSQL_DATABASE_NAME=$MYSQL_DATABASE_PREFIX$NAME
-MYSQL_DATABASE_NAME="${MYSQL_DATABASE_NAME//./_}"
-if [[ $EDITION != "custom-"* ]]; then
-    if [ -d "$DIRECTORY" ]; then
-        echo "already exists"
-        exit;
-    fi
-fi
-## Create Database
-mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE_NAME\`"
-
-COMPOSER="composer"
-PHP="php"
-if [ "$VERSION" ]; then
-    if [[ $VERSION = "2.1."* ]]; then
-        if [ "$PHP7" ]; then
-            PHP=$PHP7
-        fi
-        if [ "$COMPOSER_PHP7" ]; then
-            COMPOSER=$COMPOSER_PHP7
-        fi
-    fi
-fi
-
-## Download Magento
-$MC_COMMAND project:get $PROJECT $DIRECTORY
-cd $DIRECTORY
-$MC_COMMAND build --no-build-hooks
-
-## Install Sample Data
-mkdir $DIRECTORY/var/composer_home
-
-## Copy Json Auth
-if [ -e $COMPOSER_AUTH_JSON_FILE_PATH ]; then
-        cp $COMPOSER_AUTH_JSON_FILE_PATH $DIRECTORY/var/composer_home/auth.json
-fi
-
-## Install Magento
-URL="http://$DOMAIN"
-if [ "$secure" = "true" ]; then
-	URL="https://$DOMAIN"
-fi
-
-rm -rf $DIRECTORY/generated/*
-rm -rf $DIRECTORY/var/cache/*
-
-$PHP $DIRECTORY/bin/magento setup:install --admin-firstname="$MAGENTO_USERNAME" --admin-lastname="$MAGENTO_USERNAME" --admin-email="$MAGENTO_USER_EMAIL" --admin-user="$MAGENTO_USERNAME" --admin-password="$MAGENTO_PASSWORD" --base-url="$URL" --backend-frontname="$MAGENTO_ADMIN_URL" --db-host="127.0.0.1" --db-name="$MYSQL_DATABASE_NAME" --db-user="$MYSQL_USER" --db-password="$MYSQL_PASSWORD" --language=nl_NL --currency=EUR --timezone=Europe/Amsterdam --use-rewrites=1 --session-save=files
-
-$PHP $DIRECTORY/bin/magento setup:upgrade
-
-
-## Developer Settings
-$PHP $DIRECTORY/bin/magento deploy:mode:set developer
-$PHP $DIRECTORY/bin/magento cache:enable
-$PHP $DIRECTORY/bin/magento cache:disable layout block_html collections full_page
-### Generated PhpStorm XML Schema Validation
-mkdir -p $DIRECTORY/.idea
-$PHP $DIRECTORY/bin/magento dev:urn-catalog:generate $DIRECTORY/.idea/misc.xml
-
-. $SCRIPTPATH/src/update_settings.sh
-
-. $SCRIPTPATH/src/secure_domain.sh
-
-. $SCRIPTPATH/src/nfs.sh
-
+$MC_COMMAND auth:info
+$MC_COMMAND login --yes
+$MC_COMMAND ssh-key:add --yes
 
 # Bitbucket START
 echo Fill in the bitbucket repository name like teamname/reponame
@@ -103,7 +33,7 @@ read BITBUCKET_REPO
 if [ -z "$BITBUCKET_REPO" ]; then
   echo You forgot to fill in the bitbucket repository URL, please fill it in like teamname/reponame
   read BITBUCKET_REPO
-  if [ -z "$BITBUCKET_REPO"]; then
+  if [ -z "$BITBUCKET_REPO" ]; then
     echo Bitbucket Integration Skipped because no bitbucket repository URL was given
   fi
 fi
@@ -112,7 +42,7 @@ read BITBUCKET_KEY
 if [ -z "$BITBUCKET_KEY" ]; then
   echo You forgot to fill in the bitbucket oauth-consumer-key, please fill it in
   read BITBUCKET_KEY
-  if [ -z "$BITBUCKET_KEY"]; then
+  if [ -z "$BITBUCKET_KEY" ]; then
     echo Bitbucket Integration Skipped because no bitbucket oauth-consumer-key was given
   fi
 fi
@@ -122,12 +52,13 @@ read BITBUCKET_SECRET
 if [ -z "$BITBUCKET_SECRET" ]; then
   echo You forgot to fill in the bitbucket oauth-consumer-secret, please fill it in
   read BITBUCKET_SECRET
-  if [ -z "$BITBUCKET_SECRET"]; then
+  if [ -z "$BITBUCKET_SECRET" ]; then
     echo Bitbucket Integration Skipped because no bitbucket oauth-consumer-secret was given
   fi
 fi
 
 echo "Setting up bitbucket integration"
 $MC_COMMAND project:curl -p $PROJECT \/integrations -i -X POST -d '{"type": "bitbucket","repository": "'$BITBUCKET_REPO'","app_credentials": {"key": "'$BITBUCKET_KEY'","secret": "'$BITBUCKET_SECRET'"},"prune_branches": false,"fetch_branches": true,"build_pull_requests": true,"resync_pull_requests": true}'
-exit;
 # Bitbucket END
+
+. $SCRIPTPATH/init_cloud_env.sh "$PROJECT" "$NAME" "master"
